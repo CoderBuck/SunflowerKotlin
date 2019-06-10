6118f21c232013df98cae622066ffc6a5a5e76e5
buck <coderbuck@163.com>
2019-06-10 17:09:56 +0800

    GardenActivity

--- app/src/main/java/me/buck/sunflower_kotlin/GardenActivity.kt
+++ app/src/main/java/me/buck/sunflower_kotlin/GardenActivity.kt
@@ -1,14 +1,48 @@
 package me.buck.sunflower_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import me.buck.sunflower_kotlin.databinding.ActivityGardenBinding
 
 class GardenActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_garden)
+        val binding = DataBindingUtil.setContentView<ActivityGardenBinding>(this, R.layout.activity_garden)
+        drawerLayout=binding.drawerLayout
+        navController=Navigation.findNavController(this,R.id.garden_nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        binding.navigationView.setupWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
 }
