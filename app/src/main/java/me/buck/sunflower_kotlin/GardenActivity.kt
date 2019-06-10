c55606034e720e224c6c7b39de554f76656800d3
buck <coderbuck@163.com>
2019-06-10 12:42:56 +0800

    init

--- app/src/main/java/me/buck/sunflower_kotlin/GardenActivity.kt
+++ app/src/main/java/me/buck/sunflower_kotlin/GardenActivity.kt
@@ -3,10 +3,12 @@ package me.buck.sunflower_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GardenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_garden)
     }
+
+
 }
